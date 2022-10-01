8d117453d472b97298a30758fe7ec4ed2353b778
Alex Koleda <alxkoleda@gmail.com>
2022-10-01 10:57:27 +0300

    bounce and complaint

--- utils/sns_webhooks/email_complaint.py
+++ utils/sns_webhooks/email_complaint.py
@@ -1,24 +1,22 @@
-# import api_models.models as m
 import json
 import logging
 
 from fastapi import APIRouter
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.requests import Request
+
 # import sqlalchemy as sa
 from sqlalchemy.dialects.postgresql import insert as pg_insert
 from models.db_models import EmailAddress
 from models.enums import EmailStatusEnum
 from utils.db import async_engine
 
-# from core.db import get_session
-# from utils.utils import get_decoded_gql_id
 
 handle_email_complaint = APIRouter()
 
-
+# /handle-email-complaint
 @handle_email_complaint.post("")
-async def handle_complaint(req: Request):
+async def handle_bounce(req: Request):
     async with AsyncSession(async_engine) as session:
         logging.warning("GOT POST")
         body = await req.json()
@@ -32,20 +30,21 @@ async def handle_complaint(req: Request):
             if not msg:
                 return
             msg = json.loads(body["Message"])
-            if msg.get("bounce"):
-                for address in msg["bounce"]["bouncedRecipients"]:
+            if msg.get("complaint"):
+                for address in msg["complaint"]["complainedRecipients"]:
                     await session.execute(
-                        pg_insert(EmailAddress).values(
+                        pg_insert(EmailAddress)
+                        .values(
                             {
                                 EmailAddress.address: address["emailAddress"],
-                                EmailAddress.status: EmailStatusEnum.BOUNCED,
+                                EmailAddress.status: EmailStatusEnum.COMPLAINED,
                             }
-                        ).on_conflict_do_nothing(index_elements=["address"])
+                        )
+                        .on_conflict_do_nothing(index_elements=["address"])
                     )
         await session.commit()
-    logging.info("Add new adress to db with BOUNCED status")
+    logging.info("Add new adress to db with Complaioned status")
     return "Success"
-
 
 @handle_email_complaint.post("")
 async def handle_complaint(req: Request):
