7e2d72733c1f03659dcd717f32e57fd065deab08
Alex Koleda <alxkoleda@gmail.com>
2022-10-14 10:50:00 +0300

    logging fix

--- utils/logging_tools.py
+++ utils/logging_tools.py
@@ -11,12 +11,16 @@ async def debug_log(cls, info):
         [line.decode("utf-8") for line in hd] for hd in info.context.request.headers.raw
     ]
     headers = {ln[0]: ln[1] for ln in raw_headers if ln[0] == "authorization"}
-    data = json.dumps(
-        {
-            "query": str(cls),
-            "headers": headers,
-            "variables": info.variable_values,
-            "requested_data": [x.name for x in qh.values],
-        }
-    )
-    log.debug(data)
+
+    data = {
+        "variables": info.variable_values,
+        "query": str(cls),
+        "headers": headers,
+        "requested_data": [x.name for x in qh.values],
+    }
+    if "sort" in info.variable_values:
+        data["variables"]["sort"] = [
+            str(sort_val) for sort_val in info.variable_values["sort"]
+        ]
+
+    log.debug(json.dumps(data))
