7e941d781e99598349d1b3c90ee735084d60938f
Alex Koleda <alxkoleda@gmail.com>
2022-11-04 11:38:32 +0300

    select other places

--- gql/schema.py
+++ gql/schema.py
@@ -34,8 +34,9 @@ from gql.mutations import (
 
 
 class Query(graphene.ObjectType):
-    select_users = FilterConnectionField(UserType)
     select_places = FilterConnectionField(PlaceType)
+    select_other_places = FilterConnectionField(PlaceType)
+    select_users = FilterConnectionField(UserType)
     select_category = FilterConnectionField(CategoryType)
     select_place_images = FilterConnectionField(
         PlaceImageType, deprecation_reason="Won't be accessed directly"
